92c2d67505c2bfd40c2ed59d80d05d53a65270c0
Anthony Clever <anthonyclever@gmail.com>
2020-03-30 19:35:10 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
+import { Image, ArcaImage } from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
@@ -11,6 +11,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: `602px`, margin: `1rem auto` }}>
       <Image />
     </div>
+    <footer style={{maxWidth: `204px`, margin: `2rem auto`}}>
+      <ArcaImage />
+    </footer>
   </Layout>
 );
 
